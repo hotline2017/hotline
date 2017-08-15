a0f9b0dde212d5f1bc36d0bf739df1efa3854d3e
G014A1628\ralsoon <smallkirby423@yahoo.co.jp>
2017-08-15 11:41:57 +0900

    キー入力関係のjsを追加

---
+++ js/InputKey.js
@@ -0,0 +1,54 @@
+var InputKey = function()
+{
+    this.KeyStates    = new Array(256);
+    this.KeyPressTime = new Array(256);
+    for(var i = 0; i < this.KeyStates.Length; i++)
+    {
+        this.KeyStates[i]    = false;
+        this.KeyPressTime[i] = 0;
+    }
+};
+
+InputKey.prototype =
+{
+    update : function()
+    {
+        for(var i = 0; i < this.KeyStates.Length; i++)
+        {
+            if(this.KeyStates[i]    == true)
+            {
+                this.KeyPressTime[i] += 1;
+                continue;
+            }
+
+            if(this.KeyStates[i]    == false && 
+               this.KeyPressTime[i] != 0)
+            {
+                this.KeyPressTime[i] = 0;
+                continue;
+            }
+        }
+    },
+
+    isKeydown : function(code)
+    {
+        return this.KeyStates[code]    == true &&
+               this.KeyPressTime[code] == 0;
+    },
+
+    keydown : function(code)
+    {
+        return this.KeyStates[code] == true;
+    },
+
+    isKeyup : function(code)
+    {
+        return this.KeyStates[code]    == false &&
+               this.KeyPressTime[code] != 0;
+    },
+
+    keyup   : function(code)
+    {
+        return this.KeyStates[code] == false;
+    }
+}
