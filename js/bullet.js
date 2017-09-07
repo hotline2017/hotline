59a11f16552aa856cb35268bc0606f24bec86dad
Saeki <@saeki.com>
2017-09-07 14:15:29 +0900

    弾のjsと画像

--- js/bullet.js
+++ js/bullet.js
@@ -8,14 +8,12 @@ var Bullet = function(id, pos_x, pos_y, angle)
 {
     this.id        = id;
     this.image     = new Image();
-    this.image.src = "image/player.png";
+    this.image.src = "image/chalk.png";
     this.pos_x     = pos_x;
     this.pos_y     = pos_y;
-    this.startX    = pos_x;
-    this.startY    = pos_y;
     this.angle     = angle;
-    this.vel_x     = Math.cos(angle / 180 * Math.PI);
-    this.vel_y     = Math.sin(angle / 180 * Math.PI);
+    this.vel_x     = Math.cos(angle - Math.PI / 2) ;
+    this.vel_y     = Math.sin(angle - Math.PI / 2) ;
     this.is_alive  = true;
     this.speed     = 1;
 };
@@ -33,8 +31,7 @@ Bullet.prototype =
         ctx.save();
         ctx.translate(this.pos_x, this.pos_y);
         ctx.rotate(this.angle);
-        ctx.drawImage(this.image, 0, 0, 45, 60, -22.5, -30, 45, 60);
-
+        ctx.drawImage(this.image, 0, 0, 19, 38, -9.0, -19, 19, 38);
         ctx.restore();
     },
 
@@ -50,16 +47,15 @@ Bullet.prototype =
         this.pos_y = MathExtension.lerp(this.pos_y, y_position, 0.25);
     },
 
-    rotate        : function(mouse_x, mouse_y)
-    {
-        var dir_x = mouse_x - this.pos_x;
-        var dir_y = mouse_y - this.pos_y;
+    //rotate        : function(mouse_x, mouse_y)
+    //{
+      //  var dir_x = mouse_x - this.pos_x;
+      //  var dir_y = mouse_y - this.pos_y;
+            //  this.angle = Math.atan2(dir_y, dir_x) + Math.PI / 2;
+    //},
 
-        this.angle = Math.atan2(dir_y, dir_x) + Math.PI / 2;
-    },
-
-    receiverotate : function(angle)
-    {
-        this.angle = MathExtension.lerp(this.angle, angle, 0.25);
-    }
+    //receiverotate : function(angle)
+    //{
+    //    this.angle = MathExtension.lerp(this.angle, angle, 0.25);
+    //}
 }
